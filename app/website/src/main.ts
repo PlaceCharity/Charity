0eb5f364ad499cba006c98de761b39e3f41d7e25
Mikarific <mikerific2@gmail.com>
2024-03-26 23:58:18 -0600

    website: Add toki pona localizations.

--- app/website/src/main.ts
+++ app/website/src/main.ts
@@ -4,6 +4,14 @@ import './app.css';
 import App from './App.svelte';
 
 register('en', () => import('./locale/en.json'));
+register('en-US', () => import('./locale/en.json'));
+register('en-CA', () => import('./locale/en.json'));
+register('en-GB', () => import('./locale/en.json'));
+register('en-IE', () => import('./locale/en.json'));
+register('en-AU', () => import('./locale/en.json'));
+register('en-NZ', () => import('./locale/en.json'));
+
+register('tok', () => import('./locale/tok.json'));
 
 init({
 	fallbackLocale: 'en',
