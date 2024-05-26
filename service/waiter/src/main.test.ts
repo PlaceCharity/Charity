af35ea1c96eb5a180e67be817402aaf9cce4060b
April <april@dummy.cafe>
2024-05-26 12:57:30 +0200

    waiter: update error response in test
    why isn't this typed..? it's just unknown!

--- service/waiter/src/main.test.ts
+++ service/waiter/src/main.test.ts
@@ -12,5 +12,5 @@ const client = treaty<Waiter>(app);
 test('/user/@me should throw NotAuthenticatedError when not authenticated', async () => {
 	const { data, error } = await client.user({ id: '@me' }).get();
 	expect(error).not.toBeNull;
-	expect(error!.value).toEqual(new NotAuthenticatedError());
+	expect(error!.value).toEqual({ code: 'NOT_AUTHENTICATED' });
 });
