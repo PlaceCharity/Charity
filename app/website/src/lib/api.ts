f08513676dd4668ca16a6431152350977dc5b32b
April <april@dummy.cafe>
2024-06-22 18:30:10 +0200

    web: Initialize

--- app/website/src/lib/api.ts
+++
@@ -1,7 +0,0 @@
-import { treaty } from '@elysiajs/eden';
-import type { Waiter } from 'waiter';
-
-// TODO: Configure this in .env
-export const BASE_URL = 'http://localhost:3000';
-
-export const client = treaty<Waiter>(BASE_URL);
